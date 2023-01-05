9cd73b4148255d17e03a61299e045c99779bded1
Kipol <llirikk@gmail.com>
2023-01-05 22:46:22 +0400

    fix

--- src/sagas/watchFetchDeliveryData.js
+++ src/sagas/watchFetchDeliveryData.js
@@ -1,6 +1,7 @@
 import { takeLatest } from "redux-saga/effects";
 import fetchDeliveryDataSaga from "./fetchDeliveryDataSaga";
+import { FETCH_DELIVERY_DATA_SUCCESS } from './../actions';
 
 export default function* watchFetchDeliveryData() {
-  yield takeLatest(fetchDeliveryDataSaga);
+  yield takeLatest(FETCH_DELIVERY_DATA_SUCCESS, fetchDeliveryDataSaga);
 }
